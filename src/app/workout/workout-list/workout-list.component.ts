8ce30b0159194ac143802df1c60d029e738bfc55
David Ostler <davidtostler@gmail.com>
2017-12-12 16:08:23 -0700

    Display Workouts
    Displaying all of the workouts stored in Firestore.

--- src/app/workout/workout-list/workout-list.component.ts
+++ src/app/workout/workout-list/workout-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { WorkoutService } from '../workout.service';
+import { Workout } from '../workout-model';
 
 @Component({
   selector: 'workout-list',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-list.component.scss']
 })
 export class WorkoutListComponent implements OnInit {
+  workouts: Observable<Workout[]>;
 
-  constructor() { }
+  constructor(private workoutService: WorkoutService) { }
 
   ngOnInit() {
+    this.workouts = this.workoutService.getData();
+  }
+
+  selectWorkout(workout: Workout) {
+    console.log(workout);
+  }
+
+  createNewWorkout() {
+    console.log('TODO: Create new workout');
   }
 
 }
