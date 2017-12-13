21c009fff75be52c11cc821062d1dda3c676b20f
David Ostler <davidtostler@gmail.com>
2017-12-13 14:32:01 -0700

    Update WorkoutBuilderComponent
    Updated the workout-builder to do a lot more stuff now...uh...yarp.

--- src/app/workout/workout-builder/workout-builder.component.ts
+++ src/app/workout/workout-builder/workout-builder.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { WorkoutService } from '../workout.service';
+import { Exercises, Workout } from '../workout-model';
+
+import { days } from '../workout.days';
+import { types } from '../workout.types';
 
 @Component({
   selector: 'workout-builder',
@@ -6,10 +13,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-builder.component.scss']
 })
 export class WorkoutBuilderComponent implements OnInit {
+  types: any[] = types;
+  currentType: string;
+  exercises: Observable<Exercises[]>;
+  days: string[] = days;
+  currentDay: number = 0;
+  newWorkout: Workout = { name: '', description: '', days: {} };
 
-  constructor() { }
+  constructor(private workoutService: WorkoutService) { }
 
   ngOnInit() {
+    this.exercises = this.workoutService.getExercises();
+    this.currentType = Object.keys(types)[0];
   }
 
+  showInfo() {
+    console.log(this.currentDay);
+    console.log(this.newWorkout);
+    console.log(this.currentType);
+  }
 }
